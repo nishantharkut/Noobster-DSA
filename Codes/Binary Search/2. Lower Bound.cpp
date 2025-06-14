d1a552265f4798c630ed90500833e5b18e0875a7
Nishant Harkut <nhnishantharkut@gmail.com>
2025-06-15 01:28:22 +0530

    Add initial implementation for Lower Bound algorithm; create empty Upper Bound file

---
+++ Codes/Binary Search/2. Lower Bound.cpp
@@ -0,0 +1,25 @@
+// we have to return the index of the element which is greater than equal to the target
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+class Solution {
+
+    public:
+      //write function here 
+        {  // Your code goes here
+        
+        }
+  };
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long N;
+        cin >> N;
+        Solution ob;
+
+    }
+    return 0;
+}
